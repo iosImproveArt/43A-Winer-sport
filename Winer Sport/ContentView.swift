cb57b35a030074e1b3b681ce189038255f565161
iosImproveArt <impiosart@gmail.com>
2025-01-12 15:50:54 +0200

    init

---
+++ Winer Sport/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Winer Sport
+//
+//  Created by Improve on 12.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Winer Sport/Winer_SportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Winer_SportApp.swift
+//  Winer Sport
+//
+//  Created by Improve on 12.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Winer_SportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
